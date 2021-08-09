b327f943a382e3290fd64c5b039ed54cb5606eb0
chuukwudi <chukwudiajoku@gmail.com>
2021-08-09 20:23:32 +0100

    Run and Train working perfectly.

--- Codes/Run.py
+++ Codes/Run.py
@@ -1,4 +1,4 @@
-from DataLoader import load_data, load_pathway
+from DataLoader import load_data
 from Train import trainCoxPASNet
 
 import torch
@@ -11,8 +11,7 @@ else:
 
 dtype = torch.FloatTensor
 ''' Net Settings'''
-In_Nodes = 5567 ###number of genes
-Pathway_Nodes = 860 ###number of pathways
+In_Nodes = 130 ###number of genes
 Hidden_Nodes = 100 ###number of hidden nodes
 Out_Nodes = 30 ###number of hidden nodes in the last hidden layer
 ''' Initialize '''
@@ -22,36 +21,34 @@ num_epochs = 3000 ###for grid search
 Num_EPOCHS = 20000 ###for training
 ###sub-network setup
 Dropout_Rate = [0.7, 0.5]
-''' load data and pathway '''
-pathway_mask = load_pathway("../data/pathway_mask.csv", dtype)
 
-x_train, ytime_train, yevent_train, age_train = load_data("../data/train.csv", dtype)
-x_valid, ytime_valid, yevent_valid, age_valid = load_data("../data/validation.csv", dtype)
-x_test, ytime_test, yevent_test, age_test = load_data("../data/test.csv", dtype)
-
-opt_l2_loss = 0
-opt_lr_loss = 0
-opt_loss = torch.Tensor([float("Inf")])
-###if gpu is being used
-if torch.cuda.is_available():
-	opt_loss = opt_loss.cuda()
-
-###
-opt_c_index_va = 0
-opt_c_index_tr = 0
+x_train, ytime_train, yevent_train, age_train = load_data("../data/new_train.csv", dtype)
+x_valid, ytime_valid, yevent_valid, age_valid = load_data("../data/new_validation.csv", dtype)
+x_test, ytime_test, yevent_test, age_test = load_data("../data/new_test.csv", dtype)
 
 
 ''' Initialize '''
-Initial_Learning_Rate = 0.03
-L2_Lambda = 0.001
+#Initial_Learning_Rate = 0.03
+#L2_Lambda = 0.001
+
 
 def grid_search():
+	opt_l2_loss = 0
+	opt_lr_loss = 0
+	opt_loss = torch.Tensor([float("Inf")])
+	###if gpu is being used
+	if torch.cuda.is_available():
+		opt_loss = opt_loss.cuda()
+
+	###
+	opt_c_index_va = 0
+	opt_c_index_tr = 0
 ###grid search the optimal hyperparameters using train and validation data
 	for l2 in L2_Lambda:
 		for lr in Initial_Learning_Rate:
 			loss_train, loss_valid, c_index_tr, c_index_va = trainCoxPASNet(x_train, age_train, ytime_train, yevent_train,
-																	x_valid, age_valid, ytime_valid, yevent_valid, pathway_mask,
-																	In_Nodes, Pathway_Nodes, Hidden_Nodes, Out_Nodes,
+																	x_valid, age_valid, ytime_valid, yevent_valid,
+																	In_Nodes, Hidden_Nodes, Out_Nodes,
 																	lr, l2, num_epochs, Dropout_Rate)
 			if loss_valid < opt_loss:
 				opt_l2_loss = l2
@@ -63,13 +60,22 @@ def grid_search():
 
 
 	print("Optimal L2: ", opt_l2_loss, "Optimal LR: ", opt_lr_loss)
-	print("C-index in Test: ", c_index_te)
 
+
+# grid_search()
 
 ### train Cox-PASNet with optimal hyperparameters using train data, and then evaluate the trained model with test data
 ### Note that test data are only used to evaluate the trained Cox-PASNet
+
+# Optimal L2:  0.1 Optimal LR:  0.00075
+
+Initial_Learning_Rate = 0.00075
+L2_Lambda = 0.1
+
 loss_train, loss_test, c_index_tr, c_index_te = trainCoxPASNet(x_train, age_train, ytime_train, yevent_train,
-							x_test, age_test, ytime_test, yevent_test, pathway_mask,
-							In_Nodes, Pathway_Nodes, Hidden_Nodes, Out_Nodes,
+							x_test, age_test, ytime_test, yevent_test,
+							In_Nodes, Hidden_Nodes, Out_Nodes,
 							Initial_Learning_Rate, L2_Lambda, Num_EPOCHS, Dropout_Rate)
 
+print("C-index in Test: ", c_index_te)
+# C-index in Test:  tensor(0.6716, device='cuda:0')
